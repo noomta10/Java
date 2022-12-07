d19fb103f3b73d951617f1837bb04439744d6b8f
unknown <noomta10@gmail.com>
2022-12-07 08:24:31 +0200

    another commit

--- src/q102.java
+++ src/q102.java
@@ -1,13 +1,19 @@
-// 122. Write a Java program to find a contiguous subArray with the largest sum from a given array of integers.
+// 122. Write a Java program to find a contiguous subArray with the smallest sum from a given array of integers.
 
 public class q102 {
     public static void main(String[] args) {
-        int[] array = {-5, 2, 3, 4, -5, 1, 2};
-        int[] subArray = new int[100];
+        int[] arr = {-2, -3, 4, -3, -2, 1, 5, 3};
 
+        int sum = 0;
+        int min_sum = 0;
 
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
 
+            if (sum < min_sum)
+                min_sum = sum;
         }
+
+        System.out.println("Min sum is " + min_sum);
     }
 }
