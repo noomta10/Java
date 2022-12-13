55985fc35c603da05f696d4fa963e4636a66a968
unknown <noomta10@gmail.com>
2022-12-13 12:11:12 +0200

    another commit

--- src/q102.java
+++ src/q102.java
@@ -2,17 +2,17 @@
 
 public class q102 {
     public static void main(String[] args) {
-        int[] numbers = {-2, -3, 4, -3, -3, 1, 5, 3};
+        int[] numbers = {-2, -3, 4, -3, -3, 100, 1, -1, -100};
         int min_sum = 0;
         int max_sum = 0;
         int sum = 0;
         System.out.println("Min sum of sub array is " + FindMinSum(numbers, min_sum, sum));
-        System.out.println("Max sum of sub array is " + FindMaxSum(numbers, min_sum, sum));
+        System.out.println("Max sum of sub array is " + FindMaxSum(numbers, max_sum, sum));
     }
 
-    public static int FindMinSum(int[] numbers, int min_sum, int sum){
+    public static int FindMinSum(int[] numbers, int min_sum, int sum) {
         for (int start = 0; start < numbers.length; start++) {
-            for (int end = start; end < numbers.length; end++){
+            for (int end = start; end < numbers.length; end++) {
                 sum += numbers[end];
                 if (sum < min_sum)
                     min_sum = sum;
@@ -22,9 +22,9 @@ public class q102 {
         return min_sum;
     }
 
-    public static int FindMaxSum(int[] numbers, int max_sum, int sum){
+    public static int FindMaxSum(int[] numbers, int max_sum, int sum) {
         for (int start = 0; start < numbers.length; start++) {
-            for (int end = start; end < numbers.length; end++){
+            for (int end = start; end < numbers.length; end++) {
                 sum += numbers[end];
                 if (sum > max_sum)
                     max_sum = sum;
