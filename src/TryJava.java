3aecde0d57dc56821ff0d84436241f131dbd5c47
unknown <noomta10@gmail.com>
2022-12-18 08:31:48 +0200

    another commit

--- src/TryJava.java
+++ src/TryJava.java
@@ -1,9 +1,18 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.swing.*;
+import java.awt.*;
 
 public class TryJava {
     public static void main(String[] arg) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Hello World");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(700,700);
+        frame.setVisible(true);
+
+        ImageIcon image = new ImageIcon("img.png");
+        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(Color.CYAN);
 
     }
 }
