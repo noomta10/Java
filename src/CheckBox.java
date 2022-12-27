a443c2a2339ece78e47de2d5527c002d9f83324c
noam <noomta10@gmail.com>
2022-12-27 20:49:01 +0200

    my laptop

--- src/CheckBox.java
+++ src/CheckBox.java
@@ -6,13 +6,28 @@ import java.awt.event.ActionListener;
 public class CheckBox extends JFrame implements ActionListener {
 
     JCheckBox checkBox;
+    JButton button;
+    ImageIcon xIcon;
+    ImageIcon checkIcon;
 
     CheckBox(){
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLayout(new FlowLayout());
 
-        checkBox = new JCheckBox();
+        xIcon = new ImageIcon("x.png");
+        checkIcon = new ImageIcon("check.png");
 
+        button = new JButton("Submit");
+        button.addActionListener(this);
+
+        checkBox = new JCheckBox();
+        checkBox.setText("I'm not a robot");
+        checkBox.setFocusable(false);
+        checkBox.setFont(new Font("Comic Sans MS", Font.BOLD, 55));
+        checkBox.setIcon(xIcon);
+        checkBox.setSelectedIcon(checkIcon);
+
+        this.add(button);
         this.add(checkBox);
         this.pack();
         this.setVisible(true);
@@ -20,6 +35,8 @@ public class CheckBox extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (e.getSource() == button){
+            System.out.println(checkBox.isSelected());
+        }
     }
 }
--- src/CheckBoxMain.java
+++ src/CheckBoxMain.java
@@ -1,7 +1,5 @@
-import javax.swing.*;
-
 public class CheckBoxMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new CheckBox();
     }
 }
