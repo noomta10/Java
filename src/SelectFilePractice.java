7d0f9cb6a2cfdba9a3dae999a9eb0bf97ed49951
noam <noomta10@gmail.com>
2023-01-03 20:03:45 +0200

    my laptop

---
+++ src/SelectFilePractice.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class SelectFilePractice extends JFrame implements ActionListener {
+
+    JButton button;
+
+    SelectFilePractice() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+        button = new JButton("Select File");
+        button.addActionListener(this);
+
+        this.add(button);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setCurrentDirectory(new File(".")); //sets current directory
+
+            int response = fileChooser.showOpenDialog(null); //select file to open
+            //int response = fileChooser.showSaveDialog(null); //select file to save
+
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+
+            }
+        }
+    }
+}
