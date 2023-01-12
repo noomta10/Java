80ae9994a617ceee67b11825391341fd61551dec
unknown <noomta10@gmail.com>
2023-01-12 08:46:11 +0200

    Base laptop

---
+++ src/ThreadsPractice.java
@@ -0,0 +1,10 @@
+public class ThreadsPractice extends Thread {
+
+    @Override
+    public void run() {
+        if(this.isDaemon())
+            System.out.println("This is a daemon thread running..");
+        else
+            System.out.println("This is a user thread running..");
+    }
+}
