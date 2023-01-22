b55257a20a5859b61d2f0fc047cc22b078134cc7
noam <noomta10@gmail.com>
2023-01-22 21:03:26 +0200

    my laptop

---
+++ src/Efficiency.java
@@ -0,0 +1,11 @@
+public class Efficiency {
+    public static void main(String[] args){
+        int count, n, sum = 0;
+        for (count = 0; count < n; count++){
+            sum += count;
+        System.out.println("Sum: " + sum);
+        }
+    }
+}
+
+
