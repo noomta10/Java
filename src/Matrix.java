08a6500825f55344011dd73dc9ea8baa684ad170
unknown <noomta10@gmail.com>
2023-01-30 09:38:16 +0200

    Base laptop

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,9 +1,6 @@
 public class Matrix {
-    private int[][] matrix;
-    final int firstElement = 0;
-    final int BLACK = 250;
-    final int WHITE = 0;
-
+    private final int[][] matrix;
+    final int BLACK = 255;
 
     public Matrix(int[][] array) {
         this.matrix = new int[array.length][array[0].length];
@@ -21,8 +18,8 @@ public class Matrix {
         StringBuilder stringBuilder = new StringBuilder();
 
         for (int[] ints : this.matrix) {
-            for (int j = 0; j < this.matrix[firstElement].length; j++)
-                if (j == this.matrix[firstElement].length - 1)
+            for (int j = 0; j < this.matrix[0].length; j++)
+                if (j == this.matrix[0].length - 1)
                     stringBuilder.append(ints[j]).append("\n");
                 else
                     stringBuilder.append(ints[j]).append("\t");
@@ -31,8 +28,7 @@ public class Matrix {
         return stringBuilder.toString();
     }
 
-    public Matrix makeNegative(){
-
+    public Matrix makeNegative() {
 
         Matrix newMatrix = new Matrix(this.matrix);
 
@@ -44,8 +40,83 @@ public class Matrix {
         return newMatrix;
     }
 
-    public Matrix imageFilterAverage(){
-return null;
+    public Matrix imageFilterAverage() {
+        int neighborsNumber;
+        Matrix newMatrix = new Matrix(this.matrix.length, this.matrix[0].length);
+
+        for (int row = 0; row < newMatrix.matrix.length; row++) {
+            for (int col = 0; col < newMatrix.matrix[row].length; col++) {
+                neighborsNumber = findNeighbors(row, col, newMatrix);
+                newMatrix.matrix[row][col] /= neighborsNumber;
+            }
+        }
+
+        return newMatrix;
+    }
+
+    public Matrix rotateClockwise() {
+        Matrix newMatrix = new Matrix(this.matrix[0].length, this.matrix.length);
+        int counter = 0;
+
+        int[] cells = createArray(this);
+
+        for (int col = newMatrix.matrix[0].length - 1; col >= 0; col--) {
+            for (int row = 0; row < newMatrix.matrix.length; row++) {
+                newMatrix.matrix[row][col] = cells[counter];
+                counter++;
+            }
+        }
+
+        return newMatrix;
+    }
+
+    public Matrix rotateCounterClockwise() {
+        Matrix newMatrix = new Matrix(this.matrix[0].length, this.matrix.length);
+        int counter = 0;
+
+        int[] cells = createArray(this);
+        for (int col = 0; col < newMatrix.matrix[0].length; col++) {
+            for (int row = newMatrix.matrix.length - 1; row >= 0; row--) {
+                newMatrix.matrix[row][col] = cells[counter];
+                counter++;
+            }
+        }
+
+        return newMatrix;
+// Easier version
+//        return rotateClockwise().rotateClockwise().rotateClockwise();
+    }
+
+
+    private int findNeighbors(int row, int col, Matrix newMatrix) {
+        int neighborsNumber = 0;
+
+        for (int i = row - 1; i < row + 2; i++) {
+            for (int j = col - 1; (j < col + 2) && (i >= 0 && i < newMatrix.matrix.length); j++) {
+                if (j >= 0 && j < newMatrix.matrix[0].length) {
+                    newMatrix.matrix[row][col] += this.matrix[i][j];
+                    neighborsNumber++;
+                }
+            }
+        }
+
+        return neighborsNumber;
+    }
+
+    private int[] createArray(Matrix matrix) {
+        int counter = 0;
+        int[] cells = new int[this.matrix[0].length * this.matrix.length];
+
+        for (int[] row : this.matrix) {
+            for (int col = 0; col < this.matrix[0].length; col++) {
+                cells[counter] = row[col];
+                counter++;
+            }
+        }
+
+        return cells;
     }
 
 }
+
+
--- src/MatrixTest.java
+++ src/MatrixTest.java
@@ -1,7 +1,11 @@
 public class MatrixTest {
-    public static void main(String[] args){
-        int[][] array = {{1,2,3},{4,5,6},{7,8,9}};
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 3}, {4, 5, 6}};
         Matrix myMatrix = new Matrix(array);
-        System.out.println(myMatrix.makeNegative());
+        System.out.println(myMatrix);
+        System.out.println("\n");
+        System.out.println(myMatrix.rotateClockwise());
+        System.out.println("\n");
+        System.out.println(myMatrix.rotateCounterClockwise());
     }
 }
