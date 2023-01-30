00ec1f6b1da6de6cc6511723a45ae6193e1fb79a
unknown <noomta10@gmail.com>
2023-01-30 11:21:23 +0200

    Base laptop

---
+++ src/Ex14.java
@@ -0,0 +1,17 @@
+public class Ex14 {
+    public static int waterVolume(int[] heights) {
+        int maxIndex = 0;
+        int otherMax = 0;
+        int countWater = 0;
+
+        // find the max height in the array
+        for (int height : heights) {
+            maxIndex = Math.max(maxIndex, height);
+        }
+
+        for (int i = 0; i < maxIndex; i++) {
+
+        }
+
+    }
+}
