5b11ae3001de03a4c9262babddf54bc7f29721a1
unknown <noomta10@gmail.com>
2023-01-30 15:40:50 +0200

    Base laptop

--- src/Ex14.java
+++ src/Ex14.java
@@ -1,17 +1,37 @@
 public class Ex14 {
+    public static void main(String[] args){
+        System.out.println(waterVolume(new int[]{1,2,3,1,2,4}));
+    }
+
     public static int waterVolume(int[] heights) {
         int maxIndex = 0;
         int otherMax = 0;
         int countWater = 0;
 
-        // find the max height in the array
-        for (int height : heights) {
-            maxIndex = Math.max(maxIndex, height);
+        // Find the max height in the array
+        for (int index =0; index < heights.length; index++) {
+            if (heights[index] > maxIndex)
+                maxIndex = index;
         }
 
-        for (int i = 0; i < maxIndex; i++) {
-
+        // For the indexes to the left of the max height, the max height is their heighest to the right
+        for (int index = 0; index < maxIndex; index++) {
+            if (heights[index] > otherMax)
+                otherMax = heights[index];
+            else
+                countWater += otherMax - heights[index];
         }
 
+        otherMax = 0;
+
+        // For the indexes to the right of the max height, the max height is their heighest to the left
+        for (int index = heights.length - 1; index >= maxIndex; index--) {
+            if (heights[index] > otherMax)
+                otherMax = heights[index];
+            else
+                countWater += otherMax - heights[index];
+        }
+
+        return countWater;
     }
 }
